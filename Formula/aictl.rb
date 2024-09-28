78234c653def07ec13fc3b4a754e996aafce8d24
Akira Masuda <masuda.akira1014@gmail.com>
2024-09-28 16:12:11 +0900

    update

--- Formula/aictl.rb
+++ Formula/aictl.rb
@@ -1,9 +1,9 @@
 class Aictl < Formula
     desc "Handy CLI tool to ask anything to generative AI in command line."
     homepage "https://github.com/go-zen-chu/aictl"
-    url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.0/aictl-1.0.0.tar.gz"
-    sha256 "チ"
-    version "1.0.0"
+    url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.2/aictl_Darwin_arm64.tar.gz"
+    sha256 "23e2ec29e339db208f09bc288a5db17a886c3e04d36d758a0631db1b5a86480f"
+    version "1.0.2"
 
     def install
         bin.install "aictl"
