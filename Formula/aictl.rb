6afb29f23de81eef2312828910472adbf9687161
Akira Masuda <amasuda@lycorp.co.jp>
2024-09-28 23:34:05 +0900

    update formula to v1.0.3

--- Formula/aictl.rb
+++ Formula/aictl.rb
@@ -1,9 +1,17 @@
 class Aictl < Formula
     desc "Handy CLI tool to ask anything to generative AI in command line."
     homepage "https://github.com/go-zen-chu/aictl"
-    url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.2/aictl_Darwin_arm64.tar.gz"
-    sha256 "f0ca921cab8b403e2be46c67971f5e221d15d2e4d355f8c3f0d0b2b7e794a320"
-    version "1.0.2"
+    version "%[1]s"
+    
+    on_macos do
+        if Hardware::CPU.arm?
+            url "https://github.com/go-zen-chu/aictl/releases/download/v%[1]s/aictl_Darwin_arm64.tar.gz"
+            sha256 "cab0c349bcacddb719fd30af36a39ca91704fefe1efb8e05d17d726c66400afd"
+        else
+            url "https://github.com/go-zen-chu/aictl/releases/download/v%[1]s/aictl_Darwin_x86_64.tar.gz"
+            sha256 "3c10570fd5f9a20a11cfb04b732193b7b5d1cbc52742933ec4fd0aa11150fff6"
+        end
+    end
 
     def install
         bin.install "aictl"
@@ -13,3 +21,4 @@ class Aictl < Formula
         system "#{bin}/aictl", "--help"
     end
 end
+1.0.3
