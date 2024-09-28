7635afef951622a3de1dc24de247613fcd4ac3d3
GitHub Action <action@github.com>
2024-09-29 00:18:48 +0900

    update formula to v1.0.4

--- Formula/aictl.rb
+++ Formula/aictl.rb
@@ -1,15 +1,15 @@
 class Aictl < Formula
     desc "Handy CLI tool to ask anything to generative AI in command line."
     homepage "https://github.com/go-zen-chu/aictl"
-    version "1.0.3"
+    version "1.0.4"
     
     on_macos do
         if Hardware::CPU.arm?
-            url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.3/aictl_Darwin_arm64.tar.gz"
-            sha256 "cab0c349bcacddb719fd30af36a39ca91704fefe1efb8e05d17d726c66400afd"
+            url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.4/aictl_Darwin_arm64.tar.gz"
+            sha256 "d7703233c5a6e7c4dfad95ebc66a0a569adacdfc9a60ada335710919e02d274e"
         else
-            url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.3/aictl_Darwin_x86_64.tar.gz"
-            sha256 "3c10570fd5f9a20a11cfb04b732193b7b5d1cbc52742933ec4fd0aa11150fff6"
+            url "https://github.com/go-zen-chu/aictl/releases/download/v1.0.4/aictl_Darwin_x86_64.tar.gz"
+            sha256 "8d308147f1ce0be96c7ccd81a27b619da913384dab819e6486c2cfe723691890"
         end
     end
 
